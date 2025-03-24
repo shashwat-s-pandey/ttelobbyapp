6fef8c6f88974c28efce4657e1ef9c33f6de0150
Freeze-ssp <shashwatssp@gmail.com>
2025-03-24 22:45:46 +0530

    about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -27,6 +27,7 @@ const about = () => {
           blurDataURL="/cbvndb.jpeg" 
         />
       </motion.div>
+      <iframe src="https://osivamh5v2wvbf2.in.qlikcloud.com/single/?appid=a972b12d-666a-46ec-8a49-7dd39c6afb01&sheet=dGSNjcJ&theme=horizon&opt=ctxmenu,currsel" style={{ border: "none", width: "100%", height: "100vh" }}></iframe>
         <Footer/>
     </div>
   )
